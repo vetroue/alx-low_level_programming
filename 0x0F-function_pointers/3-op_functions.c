92af09d09ab685eaf9adafcebb3697a6ff96d237
Confidence Tshikovhi <136083432+vetroue@users.noreply.github.com>
2023-08-16 15:37:04 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,64 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
-*get_op_func - selects the correct function to perform
-*the operation asked by the user. You’re not allowed
-*to declare any other function.
-*@s: operator passed as argument
+*op_add - Returns the sum of two numbers
+*@a:the first number
+*@b:the second number
 *
-*Return: A pointer to the function corresponding to
-*the operator given in parameter
+*Return:The sum of a and b
 */
-int (*get_op_func(char *s))(int, int)
+
+int op_add(int a, int b)
 {
+return (a + b);
+}
 
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
+/**
+*op_sub - Returns the substraction of two numbers
+*@a:The first number
+*@b:The second number
+*
+*Return: The Difference of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
 
-return (ops[i].f);
+/**
+*op_mul - Returns the multiplication of two numbers
+*@a:The first number
+*@b: The second number
+*
+*Return: The multiplication of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
 
+
+/**
+*op_div - Returns the division of two numbers.
+*@a: The first number.
+*@b: The second number.
+*
+*Return: The quotient of a and b.
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+*op_mod - Returns the remainder of the division of two numbers.
+*@a: The first number.
+*@b: The second number.
+*
+*Return: The remainder of the division of a by b.
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
 }

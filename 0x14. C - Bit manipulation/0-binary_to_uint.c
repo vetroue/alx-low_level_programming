a4cb34de483134275e50db837ba44b6b13110c08
Confidence Tshikovhi <136083432+vetroue@users.noreply.github.com>
2023-09-02 14:47:18 +0200

    Update 0-binary_to_uint.c

--- 0x14. C - Bit manipulation/0-binary_to_uint.c
+++ 0x14. C - Bit manipulation/0-binary_to_uint.c
@@ -1,37 +1,33 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_uint - This funtion convert binary to decimal
+ * @b: This is my string of entry
+ * Return: This return to a digital number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int decimal = 0;
+	int multiplicador = 1, index = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (index = 0; b[index] != '\0'; index++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[index] != '0' && b[index] != '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
 	}
+	for (index = index - 1; index >= 0; index--)
+	{
+		char currentCharacter;
 
-	return (ui);
+		currentCharacter = b[index];
+		if (currentCharacter == '1')
+		{
+			decimal += multiplicador;
+		}
+		multiplicador = multiplicador * 2;
+		if (index == 0)
+			break;
+	}
+	return (decimal);
 }
